d4df8531ac6cc98596df997848ceacb07fda867c
vladpacc <48735802+vladpacc@users.noreply.github.com>
2020-11-20 11:48:19 -0600

    Update Jenkinsfile-Slack.groovy

--- Jenkinsfile-Slack.groovy
+++ Jenkinsfile-Slack.groovy
@@ -1,14 +1,13 @@
 node {
-    stage("Bulding"){
-        echo 'Bulding..'
-    }
-    stage("Testing"){
-        echo 'Testing..'
-    }
-    stage("Deploying "){
-        echo 'Deploying....'
-    }
-    stage("Send Notification to Slack"){
-        slackend color: '#BADA55', message: 'Hello, World!'
-    }
-}
+  stage("Building"){
+    echo 'Building..'
+  }
+  stage("Testing"){
+    echo 'Testing..'
+  }
+  stage("Deploying "){
+    echo 'Deploying....'
+  }
+  stage("Send Notification to Slack"){
+    slackSend color: '#BADA55', message: 'Finished deployment!'
+  }
